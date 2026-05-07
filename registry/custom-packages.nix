c00dc5203f40acd2aa968120b99c2e90d8fd73b0
omgbebebe <omgbebebe@gmail.com>
2026-05-07 22:03:31 +0400

    Update idris2-fmt to v0.13.1

--- registry/custom-packages.nix
+++ registry/custom-packages.nix
@@ -6,8 +6,8 @@ let
   fmt-src = pkgs.fetchFromGitHub {
     owner = "gvnkd";
     repo = "idris2-fmt";
-    rev = "v0.13.0";
-    hash = "sha256-j8rmiab2Nx3NjMm+UE5b/Q/4aL3Bx5qYx1slriumWa4=";
+    rev = "v0.13.1";
+    hash = "sha256-myWELf4E1sDsjciP8C5SJinqCAUdJ0oFJx2kMoWBHX0=";
   };
 
   optparse-src = pkgs.fetchFromGitHub {
@@ -33,7 +33,7 @@ in
     deps = [ "prettier" "idris2" "optparse-applicative" ];
     postPatch = ''
       # Version is auto-generated from git tags but fetchFromGitHub strips git metadata
-      sed -i 's/versionString = ".*"/versionString = "0.13.0"/' src/IdrisFmt/Version.idr
+      sed -i 's/versionString = ".*"/versionString = "0.13.1"/' src/IdrisFmt/Version.idr
     '';
   };
 
